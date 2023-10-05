3e9915e410b004da0c5ae7da98a06c8e84358681
ziaulh53 <ziaul.siu@gmail.com>
2023-10-05 21:31:07 +0600

    update profile api

--- app/Http/Controllers/Api/UserAuthController.php
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLogin;
 use App\Http\Requests\UserLogout;
 use App\Http\Requests\UserSignup;
+use App\Http\Requests\UserUpdate;
 use App\Models\User;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,15 @@ use Illuminate\Support\Facades\Password;
 
 class UserAuthController extends Controller
 {
-    public function userSignup(UserSignup $request){
+    public function userSignup(UserSignup $request)
+    {
         $data = $request->validated();
         /** @var User $user */
         User::create($data);
-        return response(['success'=>true, 'msg'=>'Registration Successfully']);
+        return response(['success' => true, 'msg' => 'Registration Successfully']);
     }
-    public function userLogin(UserLogin $request){
+    public function userLogin(UserLogin $request)
+    {
         $credential = $request->validated();
         if (!Auth::attempt($credential)) {
             return response([
@@ -27,26 +30,37 @@ class UserAuthController extends Controller
                 'success' => false
             ], 200);
         }
-         /** @var User $user */
-         $user  = Auth::user();
-         $token = $user->createToken('main')->plainTextToken;
-         $success = true;
-         return response(compact('user', 'token', 'success'));
+        /** @var User $user */
+        $user  = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        $success = true;
+        return response(compact('user', 'token', 'success'));
     }
 
-    public function userLogout(UserLogout $request){
+    public function userUpdate(UserUpdate $request)
+    {
+        $user = auth()->user();
+        $data = $request->validated();
          /** @var User $user */
-         $user = $request->user();
-         $user->currentAccessToken()->delete;
-         return response(['success'=>true]);
+        $user->update($data);
+        return response(['success'=>true, 'msg'=>'Profile info updated']);
     }
 
-    public function userResetPasswordRequest(Request $request){
+    public function userLogout(UserLogout $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete;
+        return response(['success' => true]);
+    }
+
+    public function userResetPasswordRequest(Request $request)
+    {
         /** @var User $user */
         $user = User::where('email', $request['email'])->first();
         if (!$user) {
-            return response(['success'=>false, 'msg'=>'User not found'], 404);
+            return response(['success' => false, 'msg' => 'User not found'], 404);
         }
         Password::sendResetLink($user->email);
-   }
+    }
 }
